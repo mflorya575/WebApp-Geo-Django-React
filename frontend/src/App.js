7c0fc5ae1d7b42fa9a8bf75c7780bde722f38577
Michael Florya <mih2303@mail.ru>
2024-09-29 17:03:00 +0300

    create component Fruit with props

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,10 +3,10 @@ import './App.css';
 import SecondComponent from './Components/SecondComponent';
 import React from 'react';
 import AppleComponent from './Components/AppleComponent';
+import Fruit from './Components/Fruit';
 
 function App() {
-
-  return <AppleComponent />
+  return <Fruit name="banana" />
 }
 
 export default App;
