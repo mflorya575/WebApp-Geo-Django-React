7817465d876326460d41dbb7043d49f80353a1f2
Michael Florya <mih2303@mail.ru>
2024-09-29 19:33:50 +0300

    create React Link navigate

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+
 
 function Home() {
   return (
     <div>
         <h1>This is the homepage</h1>
+        <br />
+        <Link to="/login">Login</Link>
+        <br />
+        <Link to="/listings">Listings</Link>
     </div>
   )
 }
