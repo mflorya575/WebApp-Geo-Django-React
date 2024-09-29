00102d7f3d0d9c0d0e7d093c47e964eb65fd57f6
Michael Florya <mih2303@mail.ru>
2024-09-29 20:00:52 +0300

    config mui & import to the homepage

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -1,17 +1,10 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import { Button } from '@mui/material';
 
 
 function Home() {
-  return (
-    <div>
-        <h1>This is the homepage</h1>
-        <br />
-        <Link to="/login">Login</Link>
-        <br />
-        <Link to="/listings">Listings</Link>
-    </div>
-  )
+  return <Button variant="text">Text</Button>
 }
 
 export default Home
