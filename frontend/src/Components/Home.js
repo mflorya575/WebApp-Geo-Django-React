e53759ca0a436a3aab0f5eb3594b5dd1630f0e02
Michael Florya <mih2303@mail.ru>
2024-10-04 14:42:03 +0300

    building the rest of the home page

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -5,6 +5,7 @@ import { makeStyles } from '@mui/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 
 import CustomCard from './CustomCard';
+import city from '../Assets/city.jpg';
 
 
 const useStyles = makeStyles({
@@ -37,7 +38,33 @@ const useStyles = makeStyles({
     '&:hover': {
       backgroundColor: 'green',
     },
-  }
+  },
+
+  cityImg: {
+    width: '100%',
+    height: '92vh',
+  },
+
+  overlayText: {
+    position: 'absolute',
+    zIndex: '100',
+    top: '100px',
+    left: '20px',
+    textAlign: 'center',
+  },
+
+  homeText: {
+    color: 'white',
+    fontWeight: 'bolder',
+  },
+
+  homeBtn: {
+    fontSize: '3.5rem',
+    borderRadius: '15px',
+    backgroundColor: 'green',
+    marginTop: '2rem',
+    boxShadow: '3px 3px 3px white',
+  },
 });
 
 function Home() {
@@ -67,6 +94,15 @@ function Home() {
           </div>
         </Toolbar>
       </AppBar>
+      <div style={{ position: 'relative' }}>
+        <img src={city} alt="City" className={classes.cityImg} />
+        <div className={classes.overlayText}>
+          <Typography variant='h1' className={classes.homeText}>
+            FIND YOUR <span style={{ color: 'green' }}>NEXT PROPERTY</span> ON THE TSHAN WEBSITE
+          </Typography>
+          <Button variant='contained' className={classes.homeBtn}>SEE ALL PROPERTIES</Button>
+        </div>
+      </div>
     </>
   );
 }
