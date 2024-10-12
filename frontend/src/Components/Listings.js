999b66969f4e93f72e4de2ea46cfccd2fd1fd4fc
Michael Florya <mih2303@mail.ru>
2024-10-12 17:02:12 +0300

    office map icons

--- frontend/src/Components/Listings.js
+++ frontend/src/Components/Listings.js
@@ -2,11 +2,30 @@ import React from 'react';
 
 // React leaflet
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { Icon } from 'leaflet';
+
 // Mui
 import { Grid, AppBar, Typography } from '@mui/material';
 
+// Map icons
+import houseIconPng from '../Assets/Mapicons/house.png';
+import apartmentIconPng from '../Assets/Mapicons/apartment.png';
+import officeIconPng from '../Assets/Mapicons/office.png';
+
 
 function Listings() {
+  const houseIcon = new Icon({
+    iconUrl: houseIconPng,
+    iconSize: [40, 40],
+  })
+  const apartmentIcon = new Icon({
+    iconUrl: apartmentIconPng,
+    iconSize: [40, 40],
+  })
+  const officeIcon = new Icon({
+    iconUrl: officeIconPng,
+    iconSize: [40, 40],
+  })
   return (
     <Grid container>
       <Grid item xs={4}>
@@ -30,7 +49,7 @@ function Listings() {
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
               />
-              <Marker position={[51.505, -0.09]}>
+              <Marker icon={officeIcon} position={[51.480, -0.126]}>
                 {/* <Popup>
                   A pretty CSS3 popup. <br /> Easily customizable.
                 </Popup> */}
